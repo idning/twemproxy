3818acadd4ad26e9477e0553b04f4be8226773ab
idning <idning@gmail.com>
2014-05-10 17:03:10 +0800

    setup travis-ci with test-twemproxy
    update .travis.yam
    
    update travis
    
    update travis
    
    update travis
    
    update travis
    
    update travis
    
    update travis
    
    update travis
    
    update travis
    
    update travis
    
    update travis
    
    update travis
    
    update travis
    
    update travis
    
    update travis
    
    update travis
    
    update travis
    
    update travis
    
    update travis

--- travis.sh
+++ travis.sh
@@ -3,20 +3,23 @@
 #author : ning
 #date   : 2014-05-10 16:54:43
 
-DATE=`date +'%Y%m%d%H%M'`
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#install
+sudo pip install redis
+sudo pip install nose
+sudo pip install -e git://github.com/idning/python-memcached.git#egg=memcache
+sudo apt-get install socat
 
-pip install nose
-pip install redis
-git clone https://github.com/idning/python-memcached
-cd python-memcached && python setup.py install
+#build twemproxy
+CFLAGS="-ggdb3 -O0" autoreconf -fvi && ./configure --enable-debug=log && make 
 
-
-CFLAGS="-ggdb3 -O0" autoreconf -fvi && ./configure --enable-debug=log && make && sudo make install
-
-git clone git@github.com:idning/test-twemproxy.git
+#setup test-twemproxy
+git clone https://github.com/idning/test-twemproxy.git
 
 cp  src/nutcracker  test-twemproxy/_binaries/
+cp `which redis-server` test-twemproxy/_binaries/
+cp `which redis-cli` test-twemproxy/_binaries/
+cp `which memcached` test-twemproxy/_binaries/
 
-cd test-twemproxy && nosetests -v
+#run test
+cd test-twemproxy/ && nosetests --nocapture  --nologcapture -v
 
