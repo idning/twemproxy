d3883ab396ea0453c91c633f024f8a6573e486b8
idning <idning@gmail.com>
2014-06-04 14:05:10 +0800

    add peer ip:port on notice log

--- src/nc_request.c
+++ src/nc_request.c
@@ -64,6 +64,7 @@ void
 req_notice_log(struct msg * req){
     struct msg *rsp;                /* peer message (response) */
     int64_t request_time;           /*time cost for this request*/
+    char * peer_str;                /*peer client ip:port*/
 
     uint32_t req_len = 0;           /* message length */
     uint32_t rsp_len = 0;           /* message length */
@@ -94,12 +95,14 @@ req_notice_log(struct msg * req){
 
     request_time = nc_usec_now() - req->start_usec;
 
+    peer_str = nc_unresolve_peer_desc(req->owner->sd);
+
     log_debug(LOG_NOTICE, "notice req %"PRIu64" done on c %d req_time: %"PRIi64".%03"PRIi64" type: %s "
             "narg: %"PRIu32" req_len: %"PRIu32" rsp_len: %"PRIu32" "
-            "key0: %s, done: %d, error:%d",
+            "key0: %s peer: %s done: %d error:%d",
             req->id, req->owner->sd, request_time/1000, request_time%1000, msg_type_str(req->type),
             req->narg, req_len, rsp_len,
-            req->key_start, req->done, req->error);
+            req->key_start, peer_str, req->done, req->error);
 }
 
 /*
